a5e7150e12e761446d31cf9718f6df16eaecfbd2
Natali <n.suninfire@gmail.com>
2022-12-17 16:20:58 +0200

    entities

--- dataBase/Tiding.js
+++ dataBase/Tiding.js
@@ -4,6 +4,7 @@ const {categoryEnum} = require('../constants');
 
 const tidingSchema = new Schema ({
     body: { type: String, required: true},
+    photo: { type: String, default: ''},
     category: { type: String,enum: Object.values(categoryEnum), required:true,trim: true,lowercase: true},
     user:{ type: Schema.Types.ObjectId,ref:'user',select:true},
     pub:{ type: Schema.Types.ObjectId,ref:'pub',select:true},
