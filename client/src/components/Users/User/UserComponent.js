07d9ba4a3a553a70107a8f9b9ca34c59bc4a3808
Natali <n.suninfire@gmail.com>
2023-03-28 19:24:47 +0300

    homepage

--- client/src/components/Users/User/UserComponent.js
+++ client/src/components/Users/User/UserComponent.js
@@ -2,10 +2,37 @@ import {Link} from "react-router-dom";
 import PubComponent from "../../Home/PubComponent";
 import './UserComponent.css'
 import StarRatings from "react-star-ratings/build/star-ratings";
+import {useState} from "react";
+import {registrationUser} from "../../../Services/user.service";
 
 export default function UserComponent({user}) {
     const responses = user.responses;
     const favoritePubs = user.favoritePubs;
+
+    const [showForm, setShowForm] = useState(false);
+    const [buttonName, setButtonName] = useState('Додати заклад');
+    const [pubName, setPubName] = useState('');
+    const [adminId, setAdminId] = useState('');
+    const [adminNumber, setAdminNumber] = useState('');
+    const [pubPhoto, setPubPhoto] = useState('');
+    const [pubLocation, setPubLocation] = useState('');
+    const [pubOpenTime, setPubOpenTime] = useState('');
+    const [pubClosedTime, setPubClosedTime] = useState('');
+
+    const handleClick = () => {
+        setShowForm(!showForm);
+        if (buttonName === 'Додати заклад'){
+            setButtonName('Закрити форму')
+        }else {
+            setButtonName('Додати заклад')
+        }
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        
+    }
+
 
     return (
         <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
@@ -16,7 +43,41 @@ export default function UserComponent({user}) {
                 <h2>{user.username}</h2>
                 <div className={'userAvatar'}>{user.avatar}</div>
             </div>
+                <div className={'addPub'}>
+                    <button onClick={handleClick} className={'button'}>{buttonName}</button>
+                </div>
+
             </div>
+
+            {showForm && (
+                <div className="form-wrapper">
+                    <p>*заклад буде додано лише після модерації адміністратором</p>
+                    <form onSubmit={handleSubmit}>
+                        <label htmlFor="name">Назва закладу:</label>
+                        <input type={"text"} onChange={(e) => setPubName(e.target.value)} required/>
+
+                        <label>Адміністратор закладу:</label>
+                        <input type={"text"}  placeholder={'ID'} onChange={(e) => setAdminId(e.target.value)} required/>
+
+                        <label>Номер телефону адміністратора:</label>
+                        <input type={"text"} placeholder={'+380970000000'} onChange={(e) => setAdminNumber(e.target.value)} required/>
+
+                        <label>Фото закладу:</label>
+                        <input type={"text"} onChange={(e) => setPubPhoto(e.target.value)} required/>
+
+                        <label>Адреса закладу:</label>
+                        <input type={"text"} onChange={(e) => setPubLocation(e.target.value)} required/>
+
+                        <label>Час відкриття:</label>
+                        <input type={"text"} placeholder={'00:00'} onChange={(e) => setPubOpenTime(e.target.value)} required/>
+
+                        <label>Час закриття:</label>
+                        <input type={"text"} placeholder={'00:00'} onChange={(e) => setPubClosedTime(e.target.value)} required/>
+
+                        <button type={"submit"}>Створити</button>
+                    </form>
+                </div>
+            )}
 
             <h1>Улюблені заклади</h1>
             <div style={{display: 'flex', flexDirection: 'row'}}>
@@ -35,6 +96,9 @@ export default function UserComponent({user}) {
                     <div>{response.receipt}</div>
                 </div>)}
             </div>
+
+
+
 
             {/*<div>*/}
             {/*    {user.messages}*/}
